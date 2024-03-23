f3b69094e9f8ecaf66ed2ece612960e5c8063c7d
saurabh pardeshi <saurap895955@gmail.com>
2024-03-24 04:23:08 +0530

    added quiz show screen where we will show the quiz.

---
+++ src/app/models/Questions.ts
@@ -0,0 +1,11 @@
+export class Questions{
+    question: string = "";
+    answer: string = "";
+    option1: string = "";
+    option2: string = "";
+    option3: string = "";
+    option4: string = "";
+    answerExplanation: string = "";
+    languages: string = "";
+    index!: number;
+}
--- src/app/quiz-screen/quiz-screen.component.ts
+++ src/app/quiz-screen/quiz-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Questions } from '../models/Questions';
 
 @Component({
   selector: 'app-quiz-screen',
@@ -6,5 +7,123 @@ import { Component } from '@angular/core';
   styleUrls: ['./quiz-screen.component.css']
 })
 export class QuizScreenComponent {
-
+changeQuestion(index: number) {
+  this.questionToDisplay = this.questions[index];
+//throw new Error('Method not implemented.');
+}
+  red:string="#d6a6fc"
+  blue="white"
+  questions: Questions[] = [
+    {
+        question: "What is Angular?",
+        answer: "Angular is a web application framework.",
+        option1: "A programming language.",
+        option2: "A database management system.",
+        option3: "A web application framework.",
+        option4: "An operating system.",
+        answerExplanation: "Angular is a framework for building client-side web applications.",
+        languages: "TypeScript, JavaScript",
+        index:0
+    },
+    {
+        question: "What is TypeScript?",
+        answer: "TypeScript is a superset of JavaScript.",
+        option1: "A programming language.",
+        option2: "A database management system.",
+        option3: "A web application framework.",
+        option4: "An operating system.",
+        answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+        languages: "TypeScript, JavaScript",
+        index:1
+    },
+    {
+      question: "What is TypeScript?",
+      answer: "TypeScript is a superset of JavaScript.",
+      option1: "A programming language.",
+      option2: "A database management system.",
+      option3: "A web application framework.",
+      option4: "An operating system.",
+      answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+      languages: "TypeScript, JavaScript",
+      index:3
+  },
+  {
+    question: "What is TypeScript?",
+    answer: "TypeScript is a superset of JavaScript.",
+    option1: "A programming language.",
+    option2: "A database management system.",
+    option3: "A web application framework.",
+    option4: "An operating system.",
+    answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+    languages: "TypeScript, JavaScript",
+    index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+},
+{
+  question: "What is TypeScript?",
+  answer: "TypeScript is a superset of JavaScript.",
+  option1: "A programming language.",
+  option2: "A database management system.",
+  option3: "A web application framework.",
+  option4: "An operating system.",
+  answerExplanation: "TypeScript is a statically typed language that compiles to plain JavaScript.",
+  languages: "TypeScript, JavaScript",
+  index:3
+}
+];
+questionToDisplay:Questions = this.questions[0];
 }
